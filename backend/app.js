dd0d241f69c8647a23fb12676c80f5893c257fc1
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-10-24 18:58:51 +0530

    Add pug template engine

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 const zoomMeetingsRouter = require('./routes/zoomMeetings.js');
 app.use('/zoom', zoomMeetingsRouter);
 
