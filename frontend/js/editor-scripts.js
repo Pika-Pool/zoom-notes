04dce82013d5da11cf0721365f369f9fbcf2149f
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-11-02 17:31:28 +0530

    [feat] add copy and save features(using localstorage)

--- frontend/js/editor-scripts.js
+++ frontend/js/editor-scripts.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const editor = document.querySelector('.editor--input');
+const saveBtn = document.getElementById('btn-save');
 
 document.execCommand('defaultParagraphSeparator', false, 'p');
 
@@ -8,3 +9,35 @@ function formatDoc(cmd, value) {
 	document.execCommand(cmd, false, value);
 	editor.focus();
 }
+
+function isLocalStorageAvailable() {
+	var test = 'test';
+	try {
+		localStorage.setItem(test, test);
+		localStorage.removeItem(test);
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+function saveChanges() {
+	if (isLocalStorageAvailable()) {
+		const data = {
+			[Date.now()]: editor.innerHTML,
+		};
+
+		localStorage.setItem('zoom-notes', JSON.stringify(data));
+
+		saveBtn.classList.remove('unsaved');
+	} else {
+		alert(
+			'localstorage not available in your browser' +
+				'\nTry using the latest version of google chrome or firefox'
+		);
+	}
+}
+
+editor.addEventListener('input', () => {
+	saveBtn.classList.add('unsaved');
+});
