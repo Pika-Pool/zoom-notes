016774f5248cb556b47a43e4c0d2d54737c6984f
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-11-04 16:33:35 +0530

    [feat] dave multiple notes in localstorage and view them

--- frontend/js/editor-scripts.js
+++ frontend/js/editor-scripts.js
@@ -1,43 +1,110 @@
 'use strict';
 
-const editor = document.querySelector('.editor--input');
-const saveBtn = document.getElementById('btn-save');
+let editor, saveBtn;
+let notesListDisplay, showNotesListBtn, noteNameInput;
+const storageKey = 'zoom-notes';
+let notes = null;
 
-document.execCommand('defaultParagraphSeparator', false, 'p');
+window.addEventListener('DOMContentLoaded', () => {
+	editor = document.querySelector('.editor--input');
+	saveBtn = document.getElementById('btn-save');
+
+	noteNameInput = document.getElementById('note-name--input');
+	notesListDisplay = document.querySelector('.note--list');
+	showNotesListBtn = document.querySelector('.button.note--list__show');
+
+	// list past notes
+	displayAllNotesList();
+	addNewNote();
+
+	// default seperator for content-editable
+	document.execCommand('defaultParagraphSeparator', false, 'p');
+
+	// event listeners
+	editor.addEventListener('input', () => {
+		saveBtn.classList.add('unsaved');
+	});
+	showNotesListBtn.addEventListener('click', () =>
+		notesListDisplay.classList.toggle('show')
+	);
+	notesListDisplay.addEventListener('click', changeNote);
+	saveBtn.addEventListener('click', saveNote);
+});
 
 function formatDoc(cmd, value) {
 	document.execCommand(cmd, false, value);
 	editor.focus();
 }
 
-function isLocalStorageAvailable() {
-	var test = 'test';
-	try {
-		localStorage.setItem(test, test);
-		localStorage.removeItem(test);
-		return true;
-	} catch (e) {
-		return false;
+function getNotesFromStore() {
+	const data = localStorage.getItem(storageKey);
+	if (data) {
+		return JSON.parse(data);
 	}
+
+	localStorage.setItem(storageKey, '{}');
+	return {};
 }
 
-function saveChanges() {
-	if (isLocalStorageAvailable()) {
-		const data = {
-			[Date.now()]: editor.innerHTML,
-		};
+function displayAllNotesList() {
+	notes = getNotesFromStore();
+	let notesDisplayString =
+		'<li class="note" data-addnew="true">Add New Note</li>';
+	for (const [key, value] of Object.entries(notes)) {
+		notesDisplayString += `<li class="note" data-notename="${key}">${key} - ${value.date}</li>`;
+	}
+	notesListDisplay.innerHTML = notesDisplayString;
+}
 
-		localStorage.setItem('zoom-notes', JSON.stringify(data));
+function changeNote(e) {
+	if (!e.target.classList.contains('note')) {
+		console.log('list clicked but not note');
+		return;
+	}
 
-		saveBtn.classList.remove('unsaved');
+	const noteItem = e.target;
+	if (noteItem.dataset.addnew) {
+		addNewNote();
 	} else {
-		alert(
-			'localstorage not available in your browser' +
-				'\nTry using the latest version of google chrome or firefox'
-		);
+		for (const [key, value] of Object.entries(notes)) {
+			if (key === noteItem.dataset.notename) return showNote(value);
+		}
+
+		showNote(null);
 	}
 }
 
-editor.addEventListener('input', () => {
-	saveBtn.classList.add('unsaved');
-});
+function addNewNote() {
+	editor.innerHTML = '';
+	noteNameInput.value = '';
+	noteNameInput.focus();
+
+	notesListDisplay.classList.remove('show');
+}
+
+function showNote(note) {
+	if (!note) return alert('Note not found');
+
+	noteNameInput.value = note.name;
+	editor.innerHTML = note.content;
+	editor.focus();
+
+	notesListDisplay.classList.remove('show');
+}
+
+function saveNote() {
+	const currNoteName = noteNameInput.value;
+	if (notes[currNoteName]) return alert('Note with that name already exists');
+	if (currNoteName.trim() === '') return alert('Note must have a name');
+
+	notes[currNoteName] = {
+		name: currNoteName,
+		date: new Date().toLocaleDateString(),
+		content: editor.innerHTML,
+	};
+
+	localStorage.setItem(storageKey, JSON.stringify(notes));
+	saveBtn.classList.remove('unsaved');
+
+	displayAllNotesList();
+}
